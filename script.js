e40e890a37cb2eab117fd7c9c6cd4d3e9463fb0b
Greekstu <114503006+Greekstu@users.noreply.github.com>
2026-05-21 12:04:35 -0400

    verse card first time commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const generate = document.getElementById("generator");
+const popupVerse = document.getElementById("popup-verse");
+const verseCard = document.querySelector(".verse-card");
+
+const generateCard = generate.addEventListener("click", async () => {
+ const response = await fetch('https://labs.bible.org/api/?passage=random&type=json');
+ const data = await response.json();
+ console.log(data);
+ const verse = data[0].text;
+ const book = data[0].bookname;
+ const chapter = data[0].chapter;
+ const verseNumber = data[0].verse;
+popupVerse.textContent = `${verse} - ${book} ${chapter}:${verseNumber}`;
+verseCard.classList.add("show");
+        
+});
+
+
