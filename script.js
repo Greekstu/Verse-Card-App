3cde37b3b32d4830120c4b712fa9cf88f4a180e8
Greekstu <114503006+Greekstu@users.noreply.github.com>
2026-05-28 14:28:14 -0400

    'Card shape done, aligned text, separated verse and reference'

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const generate = document.getElementById("generator");
 const popupVerse = document.getElementById("popup-verse");
+const verseReference = document.getElementById("verse-reference");
 const verseCard = document.querySelector(".verse-card");
 
 const generateCard = generate.addEventListener("click", async () => {
@@ -10,7 +11,8 @@ const generateCard = generate.addEventListener("click", async () => {
  const book = data[0].bookname;
  const chapter = data[0].chapter;
  const verseNumber = data[0].verse;
-popupVerse.textContent = `${verse} - ${book} ${chapter}:${verseNumber}`;
+popupVerse.textContent = `${verse}`;
+verseReference.textContent = `- ${book} ${chapter}:${verseNumber}`;
 verseCard.classList.add("show");
         
 });
